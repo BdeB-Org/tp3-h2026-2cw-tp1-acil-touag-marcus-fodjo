ab2bacb0accd4c72e6ca8bc8e31b28324d51fda4
Acil Touag <Acil Touag>
2026-05-19 23:11:51 -0400

    Ajout de categories.js et ventes.js

--- js/categories.js
+++ js/categories.js
@@ -1,1 +1,41 @@
+async function afficherCategories() {
+    const categories = await getAll("categorie");
+    const tableau = document.getElementById("tableau-categories");
+    tableau.innerHTML = "";
 
+    categories.forEach(categorie => {
+        const ligne = document.createElement("tr");
+        ligne.innerHTML = `
+            <td>${categorie.id}</td>
+            <td>${categorie.nom}</td>
+            <td>${categorie.description}</td>
+            <td>
+                <button class="btn-supprimer" onclick="supprimerCategorie(${categorie.id})">
+                    Supprimer
+                </button>
+            </td>
+        `;
+        tableau.appendChild(ligne);
+    });
+}
+
+document.getElementById("form-categorie").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const nouvelle = {
+        id: Date.now(),
+        nom: document.getElementById("nom").value,
+        description: document.getElementById("description").value
+    };
+
+    await create("categorie", nouvelle);
+    this.reset();
+    afficherCategories();
+});
+
+async function supprimerCategorie(id) {
+    await remove("categorie", id);
+    afficherCategories();
+}
+
+afficherCategories();
--- js/ventes.js
+++ js/ventes.js
@@ -1,1 +1,46 @@
 
+async function afficherVentes() {
+    const ventes = await getAll("vente");
+    const tableau = document.getElementById("tableau-ventes");
+    tableau.innerHTML = "";
+
+    ventes.forEach(vente => {
+        const ligne = document.createElement("tr");
+        ligne.innerHTML = `
+            <td>${vente.ID}</td>
+            <td>${vente.VOITURE_ID}</td>
+            <td>${vente.CLIENT_ID}</td>
+            <td>${vente.DATE_VENTE}</td>
+            <td>${vente.PRIX_VENTE} $</td>
+            <td>
+                <button class="btn-supprimer" onclick="supprimerVente(${vente.ID})">
+                    Supprimer
+                </button>
+            </td>
+        `;
+        tableau.appendChild(ligne);
+    });
+}
+
+document.getElementById("form-vente").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const nouvelle = {
+        id: Date.now(),
+        voiture_id: document.getElementById("voiture_id").value,
+        client_id: document.getElementById("client_id").value,
+        date_vente: document.getElementById("date_vente").value,
+        prix_vente: document.getElementById("prix_vente").value
+    };
+
+    await create("vente", nouvelle);
+    this.reset();
+    afficherVentes();
+});
+
+async function supprimerVente(id) {
+    await remove("vente", id);
+    afficherVentes();
+}
+
+afficherVentes();
